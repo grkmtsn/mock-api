ce92b33c717aea5a19653733a0bb5f0520869ae0
Görkem Tosun <grkkmm1@gmail.com>
2019-11-18 14:15:15 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,26 +1,13 @@
 var config = {
     development: {
         database: {            
-            uri: 'mongodb://<dbuser>:<dbpass>@ds139435.mlab.com:39435/gamestore',
+            uri: `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds139435.mlab.com:39435/gamestore`,
         },
         server: {
-            host: '127.0.0.1',
-            port: '8080'
+            host: process.env.HOST,
+            port: process.env.PORT
         },
-        secret: '<secretkey>'
+        secret: process.env.SECRET
     }
-    // production: {
-    //     //mongodb connection settings
-    //     database: {
-    //         host: '127.0.0.1',
-    //         port: '27017',
-    //         db: 'site'
-    //     },
-    //     //server details
-    //     server: {
-    //         host: '127.0.0.1',
-    //         port: '3421'
-    //     }
-    // }
 };
 module.exports = config;
